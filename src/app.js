f31996cc414eccb6babbebbc36cc48efa06a3fe9
Michael <mqngo21@gmail.com>
2018-07-11 21:43:21 -0700

    additional routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,24 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component.
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component.
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-
+    <div> {/* BrowerRouter takes 0-1 children, so need div to wrap routes */}
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>   {/* exact set true for exact path '/'  */}
+      <Route path="/create" component={AddExpensePage}/>
+    </div>
   </BrowserRouter>
 );
 
