4e30c103911865a8f2d7ab446ba1ac8ea6419f47
Michael <mqngo21@gmail.com>
2018-07-25 15:34:39 -0700

    setup test database env

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { AddExpensePage } from '../../components/AddExpensePage';
-import expensesExampleData from '../fixtures/expensesExampleData';
+import React from "react";
+import { shallow } from "enzyme";
+import { AddExpensePage } from "../../components/AddExpensePage";
+import expensesExampleData from "../fixtures/expensesExampleData";
 
-let addExpense, history, wrapper;
+let startAddExpense, history, wrapper;
 
-beforeEach(() => { // lifecycle method for jest used before each test 
-  addExpense = jest.fn();
+beforeEach(() => {
+  // lifecycle method for jest used before each test
+  startAddExpense = jest.fn();
   history = { push: jest.fn() };
-  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
+  wrapper = shallow(
+    <AddExpensePage startAddExpense={startAddExpense} history={history} />
+  );
 });
 
-test('should render AddExpensePage correctly', () => {
+test("should render AddExpensePage correctly", () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-test('should handle onSubmit', () => {
-  wrapper.find('ExpenseForm').prop('onSubmit')(expensesExampleData[1]);
-  expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(addExpense).toHaveBeenLastCalledWith(expensesExampleData[1]);
+test("should handle onSubmit", () => {
+  wrapper.find("ExpenseForm").prop("onSubmit")(expensesExampleData[1]);
+  expect(history.push).toHaveBeenLastCalledWith("/");
+  expect(startAddExpense).toHaveBeenLastCalledWith(expensesExampleData[1]);
 });
